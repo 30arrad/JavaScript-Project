aef8b7ce91928eaf1fc8f031b0e329f6657f3f39
Arrad Ahmed <110816803+30arrad@users.noreply.github.com>
2024-11-02 18:45:13 +0600

    Weather App

--- Day-1 Weather App/index.js
+++ Day-1 Weather App/index.js
@@ -1,1 +1,50 @@
 console.log("Weather App");
+
+// api add here
+const apiKey = "9810b574651fb71a273c42582bec543e";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+//add class name here
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+
+const weatherIcon = document.querySelector(".weather-icon");
+
+async function cheahweather(city) {
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+    var data = await response.json();
+
+    console.log(data);
+
+    // display the weather
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
+
+
+    // change the weather image according to the weather
+    if (data.weather[0].main == "Clouds") {
+        weatherIcon.src = "images/clouds.png";
+    } else if (data.weather[0].main == "Clear") {
+        weatherIcon.src = "images/clear.png";
+    } else if (data.weather[0].main == "Rain") {
+        weatherIcon.src = "images/rain.png";
+    } else if (data.weather[0].main == "Mist") {
+        weatherIcon.src = "images/mist.png";
+    } else if (data.weather[0].main == "Drizzle") {
+        weatherIcon.src = "images/drizzle.png";
+    }
+
+    document.querySelector(".weather").style.display ="block";
+}
+
+// button click
+searchBtn.addEventListener("click", () => {
+    cheahweather(searchBox.value);
+
+})
+
+
+
+
